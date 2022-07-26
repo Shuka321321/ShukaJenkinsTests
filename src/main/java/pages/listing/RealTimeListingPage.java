e893df2d5d8f0a2fb6ebfdd0cfcedb990fb6be33
Admin <Admin>
2022-07-26 08:40:29 -0700

    Changed Class Name

--- src/main/java/pages/listing/RealTimeListingPage.java
+++ src/main/java/pages/listing/RealTimeListingPage.java
@@ -7,16 +7,16 @@ import pages.base.BasePage;
 
 import static common.Config.NUMBER_OF_CARDS;
 
-public class RealtListingPage extends BasePage {
+public class RealTimeListingPage extends BasePage {
 
-    public RealtListingPage(WebDriver driver) {
+    public RealTimeListingPage(WebDriver driver) {
         super(driver);
     }
 
     private final By card
             = By.xpath("//div[@class='listing view-format']//div[@data-mode='3']");
 
-    public RealtListingPage checkCountCards() {
+    public RealTimeListingPage checkCountCards() {
         waitElementVisible(driver.findElement(card));
         int countCard = driver.findElements(card).size();
         Assert.assertEquals(countCard, NUMBER_OF_CARDS);
